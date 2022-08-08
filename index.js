933d3c368460f540c4f64b89e8c8063abbcb2b58
pv18 <pv9998286620@yandex.ru>
2022-08-08 17:17:35 +0300

    added middleware checkAuth function

--- index.js
+++ index.js
@@ -1,27 +1,130 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import mongoose from "mongoose"
+import {validationResult} from 'express-validator'
+
+import {registerValidation} from './validations/auth.js'
+
+import UserModal from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+
+mongoose
+    .connect('mongodb+srv://admin:wwwwww@cluster0.anoh8.mongodb.net/blog?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err));
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+// авторизация
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModal.findOne({email: req.body.email})
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден!'
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль!'
+            })
+        }
+
+        const token = jwt.sign({
+                _id: user._id,
+            },
+            'secret123',
+            {
+                expiresIn: '30d',
+            },
+        )
+
+        const {passwordHash, ...userData} = user._doc
+
+        res.json({
+            ...userData,
+            token
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось авторизоваться!'
+        })
+    }
 })
 
-app.post('/auth/login', (req, res) => {
-    console.log(req.body)
+// регистрация
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
 
-    const token = jwt.sign({
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+
+        const doc = new UserModal({
             email: req.body.email,
-            fullName: 'Вася Пупкин',
-        },
-        'secred123')
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash
+        })
 
-    res.json({
-        success: true,
-        token,
-    })
+        const user = await doc.save()
+
+        const token = jwt.sign({
+                _id: user._id,
+            },
+            'secret123',
+            {
+                expiresIn: '30d',
+            },
+        )
+
+        const {passwordHash, ...userData} = user._doc
+
+        res.json({
+            ...userData,
+            token
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось зарегистрироваться!'
+        })
+    }
+
+
+})
+
+app.get('/auth/me', checkAuth, async (req, res) => {
+    try {
+        const user = await UserModal.findById(req.userId)
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден!'
+            })
+        }
+
+        const {passwordHash, ...userData} = user._doc
+
+        res.json(userData)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Нет доступа!'
+        })
+    }
 })
 
 app.listen(4444, (err) => {
